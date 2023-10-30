88f918f123ce9c9f971a05d9503b139f37b65bb0
Eduar Aponte <eduaraponte8@gmail.com>
2023-10-29 21:35:05 -0300

    renderização

---
+++ js/main/script.js
@@ -0,0 +1,9 @@
+function startAnimation(card) {
+    var image = card.querySelector('img');
+    image.src = image.src; // Reinicia a animação
+}
+
+function stopAnimation(card) {
+    var image = card.querySelector('img');
+    image.src = image.src; // Para a animação
+}
