46a6eeb368f370e07e5ac53c13b37172b7820a5e
Eduar Aponte <eduaraponte8@gmail.com>
2023-11-05 23:25:21 -0300

    Atualização

--- js/main/script.js
+++ js/main/script.js
@@ -7,3 +7,8 @@ function stopAnimation(card) {
     var image = card.querySelector('img');
     image.src = image.src; // Para a animação
 }
+
+window.addEventListener('resize', function () {
+    engine.resize();
+});
+
